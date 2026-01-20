4901cf731f2a4ba707dc2ac1bb324b91e00c330c
Twigiboii546 <Gabriel.kvist.04@gmail.com>
2026-01-20 11:38:28 +0100

    nya lösningar

--- Solutions_Conditions.py
+++ Solutions_Conditions.py
@@ -26,3 +26,42 @@ def grades(n):
   grades_letters = ["F","F","D","C","B","A"]
   return grades_letters[n]
 
+## grades 100
+def grades(n):
+  grades_letters = ["A","B","C","D","F"]
+  if n >= 90:
+    return grades_letters[0]
+  if n >= 80 and n < 90:
+    return grades_letters[1]
+  if n >= 70 and n < 80:
+    return grades_letters[2]
+  if n >= 60 and n < 70:
+    return grades_letters[3]
+  else:
+    return grades_letters[4]
+## movietickets
+def movieTickets(nrTickets,nrUnder18,time):
+  price = ((nrTickets-nrUnder18)*100) + (nrUnder18*50)
+  if time >= 18:
+    return price
+  else:
+    return price*0.90
+## Pandemic shit
+def pandemic_rules(numC, totalNum, population):
+  ratio = totalNum/(population/100000)
+  if ratio >= 50 and ratio > numC:
+    return "red"
+  if ratio >= 25 and ratio < 50 and ratio > numC:
+    return "yellow"
+  if ratio <= 25:
+    return "green"
+  if ratio > numC:
+    return "green"
+  else:
+    return "green"
+
+
+#print(movieTickets(5,3,17))
+print(pandemic_rules(10,350,1000000))
+  
+
