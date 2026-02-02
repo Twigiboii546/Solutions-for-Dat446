85403dd1e7e95814bca826a47d632c0e2d8bb5e4
Twigiboii546 <Gabriel.kvist.04@gmail.com>
2026-02-02 12:51:10 +0100

    updateded version of the DNA assigment

--- DNA/lab1/aminofreq.py
+++ DNA/lab1/aminofreq.py
@@ -1,8 +1,3 @@
-
-from aminofreq import *
-
-
-
 def codons_extract(dna, frame):
     rlist = []
     dnacut = dna[frame:].upper()
@@ -10,8 +5,6 @@ def codons_extract(dna, frame):
         if i % 3 == 0:
             rlist.append(dnacut[i-3:i])
     return rlist
-
-
 
 
 def protein_extract(codons, start_codons, stop_codons):
@@ -24,17 +17,42 @@ def protein_extract(codons, start_codons, stop_codons):
                     rlist.append(codon2)
     return rlist
 
-
-
 def aa_count(codons, genetic_code):
+    rdict = {}
+    for codon in codons:
+        if codon in genetic_code:
+            aa = genetic_code[codon] #skapar alla nycklar 
+            if aa in rdict:
+                rdict[aa] += 1 #utökar med 1 om den finns i codon
+            if aa not in rdict:
+                rdict[aa] = 1 
 
+                
+    return rdict
 
-    
+def read_dna(identifier, filepath):
+    result = ""
+    reading = False
+    with open(filepath, "r") as f:
+        for line in f:
+            line = line.strip()
 
+            if line == "":
+                continue
+            if line[0] == ">":
+                if reading == True:
+                    break
+                reading = (line[i:] == identifier)
+                continue
+            if reading:
+                result += line.upper()
+    return result
+        
 
+            
+#print(aa_count(["ATG","GTG","GTC","TAG"], {'ATG':'M','TTT':'F','TTC':'F','GTC':'V','GTG':'V','AAC':'N','TAT':'Y'}))
+print(read_dna('sequence1','examples/example1.fna'))
 
-
-#def read_dna(identifier, filepath):
 
 
 #def composition(identifier,filepath,genetic_code,start_codons,stop_codons):
