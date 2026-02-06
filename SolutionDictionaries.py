81717af5b97a716abe5d3b84f46039845bae8625
filiplundgren189-create <filip.lundgren189@gmail.com>
2026-02-06 13:36:43 +0100

    Dictionaries klar

--- SolutionDictionaries.py
+++ SolutionDictionaries.py
@@ -35,3 +35,29 @@ def histogram(max_bar, labels, counts):
         count = counts[label]
         num_stars = int(count / max_count * max_bar)
         print(label.ljust(10) + "*" * num_stars)
+
+# Planet period
+import math
+
+# Constants
+M = 1.991e30  # Mass of the sun in kg
+G = 6.67e-11  # Gravitational constant in SI units
+
+# Planet radii in meters
+radii = {
+  "mercury": 57.9e9,
+  "venus": 108.2e9,
+  "earth": 149.6e9,
+  "mars": 228e9,
+  "jupiter": 779.3e9,
+  "saturn": 1427e9,
+  "uranus": 2871e9,
+  "neptune": 4497e9,
+  "pluto": 5913e9
+}
+
+def planet_period(name):
+  if name in radii:
+    print(math.sqrt((4 * math.pi**2 * radii[name]**3) / (G*M)))
+  else: 
+    print("Unknown planet name")
